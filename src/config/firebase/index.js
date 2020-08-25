1606c547d8315c3fe46034ac3c2500715a06dde2
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-25 11:37:19 +0800

    Integrasi Firebase dengan Project ReactJS (Atomic Design)

---
+++ src/config/firebase/index.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDkIZNUmxsbewCLuXnTLm0ejrTalC2CYu8",
+    authDomain: "simple-notes-firebase-705d1.firebaseapp.com",
+    databaseURL: "https://simple-notes-firebase-705d1.firebaseio.com",
+    projectId: "simple-notes-firebase-705d1",
+    storageBucket: "simple-notes-firebase-705d1.appspot.com",
+    messagingSenderId: "825992653517",
+    appId: "1:825992653517:web:763f26d5b0587bdb321784",
+    measurementId: "G-YWNW6R20XQ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+export default firebase;
